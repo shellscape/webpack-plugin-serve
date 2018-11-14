2edf328d23b1481b8b183604a79ded8a9169904c
PlayMa256 <matheus.g.silva@hotmail.com>
2018-11-14 13:54:50 -0200

    refactor: type files

--- index.d.ts
+++ index.d.ts
@@ -3,55 +3,81 @@ import * as HttpProxyMiddleware from 'http-proxy-middleware';
 import * as Koa from 'koa';
 import * as Http2 from 'http2';
 import * as Https from 'https';
+import * as zlib from "zlib";
+
+export interface CompressOptions extends zlib.ZlibOptions {
+  filter?: (content_type: string) => boolean;
+  threshold?: number
+}
+
+export interface KoaStaticOptions {
+  maxage?: number;
+  hidden?: boolean;
+  index?: string;
+  defer?: boolean;
+  gzip?: boolean;
+  br?: boolean;
+  setHeaders?: (res, path, stats) => any;
+  extensions?: Array<string> | boolean;
+}
 
 type RewriteTo = (context: Context) => string;
 
 interface Context {
-    match: RegExpMatchArray;
-    parsedUrl: Url;
+  match: RegExpMatchArray;
+  parsedUrl: Url;
 }
 
 interface Rewrite {
-    from: RegExp;
-    to: string | RegExp | RewriteTo;
+  from: RegExp;
+  to: string | RegExp | RewriteTo;
 }
 
 interface HistoryApiFallback {
-    disableDotRule?: true;
-    htmlAcceptHeaders?: string[];
-    index?: string;
-    logger?: typeof console.log;
-    rewrites?: Rewrite[];
-    verbose?: boolean;
+  disableDotRule?: true;
+  htmlAcceptHeaders?: string[];
+  index?: string;
+  logger?: typeof console.log;
+  rewrites?: Rewrite[];
+  verbose?: boolean;
 }
 
 interface Builtins {
-    proxy: (args: HttpProxyMiddleware.Config) => void;
+  proxy: (args: HttpProxyMiddleware.Config) => HttpProxyMiddleware.Proxy;
+  compress: (opts: CompressOptions) => void;
+  static: (opts: KoaStaticOptions) => void;
+  historyFallback: (opts: HistoryApiFallback) => void;
+  websocket: () => void;
 }
 
-export interface WebpackPluginServeOptions {
+declare module 'webpack-plugin-serve' {
+  interface WebpackPluginServeOptions {
     client?: {
-        address: string;
+      address: string;
     };
     compress?: boolean;
     historyFallback?: boolean | HistoryApiFallback;
     hmr?: boolean;
-    host?: string | (() => string);
+    host?: string | (() => Promise<string>) | (() => string);
     http2?: boolean | Http2.ServerOptions | Http2.SecureServerOptions;
     https?: Https.ServerOptions;
     liveReload?: boolean;
-    log?: { 
-        level: string;
-        timestamp?: boolean;
+    log?: {
+      level: string;
+      timestamp?: boolean;
     };
     middleware?: (app: Koa, builtins: Builtins) => void;
-    open?: boolean | {};
-    port?: number | (() => number);
+    open?: boolean | {
+      wait?: boolean;
+      app?: string | ReadonlyArray<string>;
+    };
+    port?: number | (() => Promise<number>) | (() => number);
     progress?: boolean | 'minimal';
     static?: string | Array<string>;
     status?: boolean;
-}
+  }
 
-export declare class WebpackPluginServe {
+  export declare class WebpackPluginServe {
     constructor(opts?: WebpackPluginServeOptions);
+  }
 }
